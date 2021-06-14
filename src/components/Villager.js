a8c492a8d8314e051ce323c2018ae520fd4b2950
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2021-06-14 01:58:00 -0400

    Setup automatic form submission

--- src/components/Villager.js
+++ src/components/Villager.js
@@ -1,5 +1,5 @@
 import getRandomVillager from "../services/getRandomVillager";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Score from "./Score";
 import Result from "./Result";
 
@@ -9,13 +9,32 @@ const Villager = (props) => {
   const [score, setScore] = useState(0);
   const [result, setResult] = useState("");
   const [resultClassName, setResultClassName] = useState("");
+  const submitButtonRef = useRef(null);
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
-    getRandomVillager().then((villager) => setVillager(villager));
+    getRandomVillager().then((randomVillager) => setVillager(randomVillager));
   }, []);
 
+  useEffect(() => {
+    const countdown = setTimeout(() => {
+      setTimeLeft(timeLeft - 1);
+      if (timeLeft === 1) {
+        submitButtonRef.current.click();
+      }
+    }, 1000);
+    return () => clearTimeout(countdown);
+  }, [timeLeft]);
+
+  const handleChange = (e) => {
+    setGuess(e.target.value);
+  }
+  
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Disable further submissions
+    setIsDisabled(true);
     // Compare guess to data
     if (guess === villager["name"]["name-USen"]) {
       // If correct, increase score
@@ -25,13 +44,18 @@ const Villager = (props) => {
       // Show result
       setResultClassName("correct");
       setResult("Correct");
-      // Remove result after a few seconds
+      // Prepare for a new round
       setTimeout(() => {
+        // Reset result
         setResultClassName("");
         setResult("");
+        // Allow submissions
+        setIsDisabled(false);
       }, 2000);
       // Change villager
-      getRandomVillager().then((villager) => setVillager(villager));
+      getRandomVillager().then((randomVillager) => setVillager(randomVillager));
+      // Reset countdown
+      setTimeLeft(12);
     } else {
       // Otherwise, end the game
       setResultClassName("wrong");
@@ -47,8 +71,8 @@ const Villager = (props) => {
       <Score score={score} />
       {villager["icon_uri"] ? <img src={villager["icon_uri"]} alt="villager" /> : <p className="my-3">Loading...</p>}
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter the villager's name..." name="guess" value={guess} onChange={(e) => setGuess(e.target.value)} />
-        <button className="btn btn-primary" type="submit">Submit</button>
+        <input type="text" placeholder="Enter the villager's name" name="guess" value={guess} onChange={handleChange} />
+        <input type="submit" ref={submitButtonRef} className="btn btn-primary btn-lg" disabled={isDisabled} value={isDisabled ? "Submitted" : `Submit (${timeLeft})`} />
       </form>
       {result === "" ? null : <Result resultClassName={resultClassName} result={result} />}
     </div>
