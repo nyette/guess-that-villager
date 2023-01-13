68399d7e0214b03070a11d9f0a0d3621473abee1
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2023-01-13 02:41:38 -0500

    Upgraded dependencies. Added Cypress.

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      return config;
+    },
+  },
+});
---
+++ cypress/e2e/game.cy.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+describe("the game", () => {
+  beforeEach(() => {
+    cy.visit("https://guess-that-villager.onrender.com/");
+  });
+
+  it("allows the user to continue, if their guess is correct", () => {
+    cy.intercept("https://acnhapi.com/v1/villagers/*", {
+      fixture: "villager",
+    }).as("getVillager");
+    cy.contains("Play").click();
+    cy.wait("@getVillager");
+    cy.get("input").should("be.visible");
+    cy.get("input").should("have.value", "");
+    cy.get("input").type("Pietro");
+    cy.get("input").should("have.value", "Pietro");
+    cy.contains("Submit").click();
+    cy.contains("Correct").should("be.visible");
+    cy.contains("Start Next Round").should("be.visible");
+  });
+
+  it("allows the user to replay, if their guess is wrong", () => {
+    cy.contains("Play").click();
+    cy.contains("Submit").click();
+    cy.contains("Game Over").should("be.visible");
+    cy.contains("Replay").should("be.visible");
+    cy.contains("Replay").click();
+    cy.get("input").should("be.visible");
+    cy.get("input").should("have.value", "");
+    cy.contains("Submit").click();
+    cy.contains("Game Over").should("be.visible");
+  });
+
+  it("allows the user to return to the main menu, if there is an error", () => {
+    cy.intercept("https://acnhapi.com/v1/villagers/*", { statusCode: 404 }).as(
+      "getVillager"
+    );
+    cy.contains("Play").click();
+    cy.wait("@getVillager");
+    cy.contains("Error").should("be.visible");
+    cy.contains("Return To Main Menu").should("be.visible");
+    cy.contains("Return To Main Menu").click();
+    cy.contains("Guess That Villager").should("be.visible");
+  });
+});
