06a8d1fd14894fea894ddd696f334439a67f9620
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2023-01-20 00:45:46 -0500

    Added Redux

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,17 @@
-import { useContext } from "react";
-import GameContext from "../contexts/game";
+import { useDispatch } from "react-redux";
+import { play, startNextRound, replay, quit } from "../slices/game";
 
 const Button = ({ content }) => {
-  const { setGame } = useContext(GameContext);
+  const dispatch = useDispatch();
 
   const handleClick = () => {
-    setGame({
-      type: content.toUpperCase(),
-    });
+    const mapping = {
+      Play: play,
+      "Start Next Round": startNextRound,
+      Replay: replay,
+      Quit: quit,
+    };
+    dispatch(mapping[content]());
   };
 
   return (
