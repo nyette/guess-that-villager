84be6a797fa96bdee870126f69ed584445418170
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2023-02-08 13:43:07 -0500

    Refactored Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import styled from "styled-components";
 import { modes } from "../slices/settings";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faSun,
+  faMoon,
   faPlay,
   faRepeat,
   faArrowRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
+import { enableLightMode, enableDarkMode } from "../slices/settings";
 import { play, startNextRound, replay, quit } from "../slices/game";
 
 export const StyledButton = styled.button`
@@ -22,12 +25,25 @@ export const StyledButton = styled.button`
   padding: 1rem;
 `;
 
+const StyledSettingsButton = styled(StyledButton)`
+  border-radius: 50%;
+  border-style: none;
+`;
+
 const Button = ({ content }) => {
   const settings = useSelector((state) => state.settings);
 
   const dispatch = useDispatch();
 
   const mapping = {
+    "Enable Light Mode": {
+      cb: enableLightMode,
+      icon: faSun,
+    },
+    "Enable Dark Mode": {
+      cb: enableDarkMode,
+      icon: faMoon,
+    },
     Play: {
       cb: play,
       icon: faPlay,
@@ -54,11 +70,22 @@ const Button = ({ content }) => {
     return mapping[content].icon;
   };
 
-  return (
-    <StyledButton dark={settings.mode === "dark"} onClick={handleClick}>
-      <FontAwesomeIcon icon={handleIcon()} /> {content}
-    </StyledButton>
-  );
+  if (content.startsWith("Enable")) {
+    return (
+      <StyledSettingsButton
+        dark={settings.mode === "dark"}
+        onClick={handleClick}
+      >
+        <FontAwesomeIcon icon={handleIcon()} />
+      </StyledSettingsButton>
+    );
+  } else {
+    return (
+      <StyledButton dark={settings.mode === "dark"} onClick={handleClick}>
+        <FontAwesomeIcon icon={handleIcon()} /> {content}
+      </StyledButton>
+    );
+  }
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import styled from "styled-components";
-import { StyledButton } from "./Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { useSelector, useDispatch } from "react-redux";
-import { enableLightMode, enableDarkMode } from "../slices/settings";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 const StyledHeader = styled.header`
   align-items: center;
@@ -12,40 +9,16 @@ const StyledHeader = styled.header`
   padding: 1rem;
 `;
 
-const SettingsButton = styled(StyledButton)`
-  border-radius: 50%;
-  border-style: none;
-`;
-
 const Header = () => {
   const settings = useSelector((state) => state.settings);
 
-  const dispatch = useDispatch();
-
-  const mapping = {
-    dark: {
-      cb: enableLightMode,
-      icon: faSun,
-    },
-    light: {
-      cb: enableDarkMode,
-      icon: faMoon,
-    },
-  };
-
-  const switchMode = () => {
-    dispatch(mapping[settings.mode].cb());
-  };
-
-  const switchIcon = () => {
-    return mapping[settings.mode].icon;
-  };
-
   return (
     <StyledHeader>
-      <SettingsButton dark={settings.mode === "dark"} onClick={switchMode}>
-        <FontAwesomeIcon icon={switchIcon()} />
-      </SettingsButton>
+      <Button
+        content={
+          settings.mode === "dark" ? "Enable Light Mode" : "Enable Dark Mode"
+        }
+      />
     </StyledHeader>
   );
 };
